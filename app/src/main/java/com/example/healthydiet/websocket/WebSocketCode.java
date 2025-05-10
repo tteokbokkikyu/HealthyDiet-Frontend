256ed308c8d11957a704b1b225ae75a87ea27680
sxm <59765936+windfall2321@users.noreply.github.com>
2025-05-10 22:36:24 +0800

    websocket前端配置

---
+++ app/src/main/java/com/example/healthydiet/websocket/WebSocketCode.java
@@ -0,0 +1,12 @@
+package com.example.healthydiet.websocket;
+
+public enum WebSocketCode {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+
+   
+}
+
---
+++ app/src/main/java/com/example/healthydiet/websocket/WebSocketMessageType.java
@@ -0,0 +1,8 @@
+package com.example.healthydiet.websocket;
+
+public class WebSocketMessageType {
+    public static final String LOGIN = "login";
+    public static final String REGISTER = "register";
+   
+    // 添加其他消息类型...
+} 
