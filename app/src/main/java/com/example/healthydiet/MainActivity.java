fddc0d23e3073e7fec3e1c385deb1cc1a9a520d0
shiningxyone <2249027756@qq.com>
2025-05-13 08:23:31 +0800

    init

--- app/src/main/java/com/example/healthydiet/MainActivity.java
+++ app/src/main/java/com/example/healthydiet/MainActivity.java
@@ -8,8 +8,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.hd2.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
